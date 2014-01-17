037668020939f22036bbfc1dfce004fd64ce9d81
Stefan Paetow <stefan.paetow@diamond.ac.uk>
2014-01-17 11:45:31 +0000

    Modified the ECP auth client to use the DARIAH client as a base. No point in reinventing the wheel now that we have a wheel to borrow.

--- README.md
+++ README.md
@@ -8,9 +8,19 @@ A Shibboleth ECP authentication client that also exposes access to the SAML resp
 Copyrights
 ----------
 
-This client is Copyright 2013, Diamond Light Source Limited, All Rights Reserved. It is released to you
+This client is Copyright 2013-2014, Diamond Light Source Limited, All Rights Reserved. It is released to you
   under the Apache License Version 2.0.
 
-The core of this client is Copyright 2013, Ubiquitous Knowledge Processing (UKP) Lab, 
-  Technische Universität Darmstadt, All Rights Reserved. The origins of this code are at: 
-  https://github.com/reckart/shib-http-client
+The core of this client is Copyright 2013-2014, Ubiquitous Knowledge Processing (UKP) Lab, 
+  Technische Universität Darmstadt, All Rights Reserved.
+
+Acknowledgement
+---------------
+
+Portions of this project were funded by the German Federal Ministry of Education and Research (BMBF) under 
+the promotional reference 01UG1110D as part of DARIAH-DE (Digital Research Infrastructure for the Arts and 
+Humanities, http://de.dariah.eu).
+
+Portions of this project have been funded by the European Commission under the 7th Framework programme Grant 
+Agreement RI-283556 and 283745 as part of the Umbrella system, a joint project of the European Photon and 
+Neutron sources, the PaNdata project, the CRISP project and EuroFEL.
--- src/main/java/uk/ac/diamond/shibbolethecpauthclient/ShibbolethECPAuthClient.java
+++ src/main/java/uk/ac/diamond/shibbolethecpauthclient/ShibbolethECPAuthClient.java
@@ -24,59 +24,33 @@
 
 package uk.ac.diamond.shibbolethecpauthclient;
 
-import static java.util.Arrays.asList;
+import static uk.ac.diamond.shibbolethecpauthclient.Utils.xmlToString;
 
-import java.io.ByteArrayInputStream;
 import java.io.IOException;
-import java.net.ProxySelector;
-import java.security.GeneralSecurityException;
 import java.util.List;
 
 import javax.security.sasl.AuthenticationException;
 
-import org.apache.log4j.Logger;
 import org.apache.http.Header;
-import org.apache.http.HttpException;
+import org.apache.http.HttpHeaders;
 import org.apache.http.HttpHost;
-import org.apache.http.HttpRequest;
-import org.apache.http.HttpRequestInterceptor;
 import org.apache.http.HttpResponse;
-import org.apache.http.client.config.CookieSpecs;
-import org.apache.http.client.config.RequestConfig;
 import org.apache.http.client.methods.HttpGet;
-import org.apache.http.client.methods.HttpHead;
 import org.apache.http.client.methods.HttpPost;
-import org.apache.http.client.methods.HttpRequestWrapper;
-import org.apache.http.config.Registry;
-import org.apache.http.config.RegistryBuilder;
-import org.apache.http.conn.socket.ConnectionSocketFactory;
-import org.apache.http.conn.socket.PlainConnectionSocketFactory;
-import org.apache.http.conn.ssl.SSLConnectionSocketFactory;
-import org.apache.http.conn.ssl.SSLContextBuilder;
-import org.apache.http.conn.ssl.TrustSelfSignedStrategy;
-import org.apache.http.cookie.Cookie;
-import org.apache.http.entity.ContentType;
 import org.apache.http.entity.StringEntity;
-import org.apache.http.impl.client.BasicCookieStore;
-import org.apache.http.impl.client.CloseableHttpClient;
-import org.apache.http.impl.client.HttpClients;
-import org.apache.http.impl.conn.PoolingHttpClientConnectionManager;
-import org.apache.http.impl.conn.SystemDefaultRoutePlanner;
-import org.apache.http.protocol.HttpContext;
 import org.apache.http.util.EntityUtils;
-
-import org.opensaml.DefaultBootstrap;
-import org.opensaml.common.xml.SAMLConstants;
+import org.apache.log4j.Logger;
+import org.opensaml.saml2.core.Response;
 import org.opensaml.saml2.core.StatusCode;
-import org.opensaml.saml2.ecp.Response;
 import org.opensaml.ws.soap.client.SOAPClientException;
 import org.opensaml.ws.soap.soap11.Body;
 import org.opensaml.ws.soap.soap11.Envelope;
 import org.opensaml.ws.soap.soap11.impl.EnvelopeBuilder;
 import org.opensaml.xml.ConfigurationException;
 import org.opensaml.xml.XMLObject;
-import org.opensaml.xml.parse.BasicParserPool;
 import org.opensaml.xml.util.Base64;
+
+import de.tudarmstadt.ukp.shibhttpclient.ShibHttpClient;
 
 /**
  * Implementation of an authentication client that uses the Shibboleth
@@ -87,29 +61,10 @@ import org.opensaml.xml.util.Base64;
  * @author Stefan Paetow
  * @since 1.0
  */
-public class ShibbolethECPAuthClient {
+public class ShibbolethECPAuthClient extends ShibHttpClient {
 
 	/** The logger instance */
 	private static final Logger log = Logger.getLogger(ShibbolethECPAuthClient.class);
-
-	private static final String AUTH_IN_PROGRESS = ShibbolethECPAuthClient.class.getName()
-			+ ".AUTH_IN_PROGRESS";
-
-	private static final String MIME_TYPE_PAOS = "application/vnd.paos+xml";
-
-	private static final String HEADER_AUTHORIZATION = "Authorization";
-
-	private static final String HEADER_CONTENT_TYPE = "Content-Type";
-
-	private static final String HEADER_ACCEPT = "Accept";
-
-	private static final String HEADER_PAOS = "PAOS";
-
-	/** The client that provides the HTTP transport for ECP to function */
-	private CloseableHttpClient client;
-
-	/** A cookie store, otherwise no cookies can be stored for the HTTP context */
-	private BasicCookieStore cookieStore;
 
 	/** The Shibboleth Identity Provider to use for authentication */
 	private String IdP;
@@ -117,25 +72,13 @@ public class ShibbolethECPAuthClient {
 	/** The Shibboleth-protected web endpoint used for initiation of authentication */
 	private String SP;
 
-	/** A parser pool to manage the SOAP and SAML message processing */
-	private BasicParserPool parserPool;
-
-	/** 
-	 * An optional, explicit proxy host. Glassfish does not necessarily allow 
-	 * the default proxy selector to pick up the proxy hosts specified on the 
-	 * JVM command-line. 
-	 */
-	private HttpHost proxyHost;
-
-	private static final List<String> REDIRECTABLE = asList("HEAD", "GET");
-
 	/**
 	 * @param proxy The proxy to initialize this client with. All connections will be proxied
 	 *  through this host 
 	 * @param idpURL The full ECP Profile URL of the Shibboleth Identity Provider
 	 * @param spURL The URL to connect to to initiate the authentication process
 	 * @param anyCert If true, disables certificate verification. Used generally with 
-	 *self-signed certificates
+	 *  self-signed certificates
 	 * 
 	 * @throws ConfigurationException
 	 * thrown if SAML is not able to initialize properly
@@ -145,79 +88,17 @@ public class ShibbolethECPAuthClient {
 	public ShibbolethECPAuthClient(HttpHost proxy, String idpURL, String spURL, boolean anyCert) 
 			throws ConfigurationException, IllegalStateException {
 
-		setIDP(idpURL);
-		setSP(spURL);
-		setProxy(proxy);
-
-		// Use a pooling connection manager, because we'll have to do a call out to the IdP
-		// while still being in a connection with the SP
-		PoolingHttpClientConnectionManager connMgr;
-		if (anyCert) {
-			try {
-				SSLContextBuilder builder = new SSLContextBuilder();
-				builder.loadTrustMaterial(null, new TrustSelfSignedStrategy());
-				Registry<ConnectionSocketFactory> socketFactoryRegistry = RegistryBuilder
-						.<ConnectionSocketFactory> create()
-						.register("http", new PlainConnectionSocketFactory())
-						.register("https", new SSLConnectionSocketFactory(builder.build()))
-						.build();
-				connMgr = new PoolingHttpClientConnectionManager(socketFactoryRegistry);
-			}
-			catch (GeneralSecurityException e) {
-				// There shouldn't be any of these exceptions, because we do not use an actual keystore
-				throw new IllegalStateException(e);
-			}
-		}
-		else {
-			connMgr = new PoolingHttpClientConnectionManager();
-		}
-		connMgr.setMaxTotal(10);
-		connMgr.setDefaultMaxPerRoute(5);
-
-		// The client needs to remember the auth cookie
-		cookieStore = new BasicCookieStore();
-		RequestConfig globalRequestConfig = RequestConfig.custom()
-				.setCookieSpec(CookieSpecs.BROWSER_COMPATIBILITY)
-				.build();
-
-		// build our client
-		if (proxyHost == null) {
-			client = HttpClients.custom()
-					.setConnectionManager(connMgr)
-					// use the proxy settings of the JVM, if specified 
-					.setRoutePlanner(new SystemDefaultRoutePlanner(ProxySelector.getDefault()))
-					// The client needs to remember the auth cookie
-					.setDefaultRequestConfig(globalRequestConfig)
-					.setDefaultCookieStore(cookieStore)
-					// Add the ECP/PAOS headers - needs to be added first so the cookie we get from
-					// the authentication can be handled by the RequestAddCookies interceptor later
-					.addInterceptorFirst(new HttpRequestPreprocessor())
-					.build();
-		}
-		else {
-			client = HttpClients.custom()
-					.setConnectionManager(connMgr)
-					// use the explicit proxy
-					.setProxy(proxyHost)
-					// The client needs to remember the auth cookie
-					.setDefaultRequestConfig(globalRequestConfig)
-					.setDefaultCookieStore(cookieStore)
-					// Add the ECP/PAOS headers - needs to be added first so the cookie we get from
-					// the authentication can be handled by the RequestAddCookies interceptor later
-					.addInterceptorFirst(new HttpRequestPreprocessor())
-					.build();
-		}
-
-		DefaultBootstrap.bootstrap();
-		parserPool = new BasicParserPool();
-		parserPool.setNamespaceAware(true);
+		// No need for IdP, username or password. They're only required for transparent auth, which we don't do 
+		super(null, null, null, proxy, anyCert, false);
+		this.IdP = idpURL;
+		this.SP = spURL;
 	}
 
 	/**
 	 * @param idpURL The full ECP Profile URL of the Shibboleth Identity Provider
 	 * @param spURL The URL to connect to to initiate the authentication process
 	 * @param anyCert If true, disables certificate verification. Used generally with 
-	 *self-signed certificates
+	 *  self-signed certificates
 	 * 
 	 * @throws ConfigurationException
 	 * thrown if SAML is not able to initialize properly
@@ -226,19 +107,7 @@ public class ShibbolethECPAuthClient {
 	 */
 	public ShibbolethECPAuthClient(String idpURL, String spURL, boolean anyCert) 
 			throws ConfigurationException, IllegalStateException {
-		this(null, idpURL, spURL, true);
-	}
-
-	private void setIDP(String idpURL) {
-		IdP = idpURL;
-	}
-
-	private void setSP(String spURL) {
-		SP = spURL;
-	}
-
-	private void setProxy(HttpHost proxy) {
-		proxyHost = proxy;
+		this(null, idpURL, spURL, anyCert);
 	}
 
 	/**
@@ -260,124 +129,78 @@ public class ShibbolethECPAuthClient {
 	public org.opensaml.saml2.core.Response authenticate(String username, String password)
 			throws IOException, AuthenticationException, SOAPClientException {
 
-		HttpGet req = new HttpGet(SP);
-		try {
-			HttpResponse res = client.execute(req);
-			log.info("HttpResponse::Status: " + res.getStatusLine());
-			log.debug("HttpResponse::res: " + res.toString());
-			Header headers[] = res.getAllHeaders();
-			for (Header h:headers) {
-				log.debug(h.getName() + ": " + h.getValue());
-			}
-			String entity = EntityUtils.toString(res.getEntity());  // Warning: This closes the getEntity() InputStream!!
-			log.debug("HttpResponse::Content: " + entity);
-
-			boolean isSamlSoap = false;
-			if (res.getFirstHeader(HEADER_CONTENT_TYPE) != null) {
-				ContentType contentType = ContentType.parse(res.getFirstHeader(HEADER_CONTENT_TYPE)
-						.getValue());
-				isSamlSoap = MIME_TYPE_PAOS.equals(contentType.getMimeType());
-			}
-
-			// We didn't receive a SOAP request back, so ECP is not enabled. This doesn't help us. Bail immediately!
-			if (!isSamlSoap) {
-				throw new SOAPClientException("Service Provider not configured to accept ECP messages");
-			}
-
-			// -- Parse PAOS response -------------------------------------------------------------
-			Envelope initialLoginSoapResponse = (Envelope) Utils.unmarshallMessage(parserPool,
-					new ByteArrayInputStream(entity.getBytes())); // turn the string output of getEntity() back into InputStream
-
-			log.debug("Logging into IdP [" + IdP + "]");
-			Envelope idpLoginSoapRequest = new EnvelopeBuilder().buildObject();
-			Body b = initialLoginSoapResponse.getBody();
-			b.detach();
-			idpLoginSoapRequest.setBody(b);
-
-			// Try logging in to the IdP using HTTP BASIC authentication
-			HttpPost idpLoginRequest = new HttpPost(IdP);
-			idpLoginRequest.getParams().setBooleanParameter(AUTH_IN_PROGRESS, true);
-			idpLoginRequest.addHeader(HEADER_AUTHORIZATION, "Basic " + Base64.encodeBytes((username + ":" + password).getBytes()));
-			idpLoginRequest.setEntity(new StringEntity(Utils.xmlToString(idpLoginSoapRequest)));
-			HttpResponse idpLoginResponse = client.execute(idpLoginRequest);
-
-			// -- Handle log-in response from the IdP --------------------------------------------
-			log.debug("Status: " + idpLoginResponse.getStatusLine());
-			if (idpLoginResponse.getStatusLine().getStatusCode() != 200) {
-				throw new AuthenticationException(idpLoginResponse.getStatusLine().toString());
-			}
-
-			String idpEntity = EntityUtils.toString(idpLoginResponse.getEntity()); // Warning: This closes the getEntity() InputStream!!
-			log.debug("HttpResponse::Content: " + idpEntity);
-
-			Envelope idpLoginSoapResponse = (Envelope) Utils.unmarshallMessage(parserPool,
-					new ByteArrayInputStream(idpEntity.getBytes())); // turn the string output of getEntity() back into InputStream
-			EntityUtils.consume(idpLoginResponse.getEntity());
-			String assertionConsumerServiceURL = ((Response) idpLoginSoapResponse.getHeader()
-					.getUnknownXMLObjects(Response.DEFAULT_ELEMENT_NAME).get(0))
-					.getAssertionConsumerServiceURL();
-			log.debug("assertionConsumerServiceURL: " + assertionConsumerServiceURL);
-
-			List<XMLObject> responses = idpLoginSoapResponse.getBody().getUnknownXMLObjects(
-					org.opensaml.saml2.core.Response.DEFAULT_ELEMENT_NAME);
-			if (!responses.isEmpty()) {
-				org.opensaml.saml2.core.Response response = (org.opensaml.saml2.core.Response) responses
-						.get(0);
-
-				// Get root code (?)
-				StatusCode sc = response.getStatus().getStatusCode();
-				while (sc.getStatusCode() != null) {
-					sc = sc.getStatusCode();
-				}
-
-				// Hm, they don't like us
-				if ("urn:oasis:names:tc:SAML:2.0:status:AuthnFailed".equals(sc.getValue())) {
-					throw new AuthenticationException(sc.getValue());
-				}
-
-				// return the SAML response we got
-				return response;
-			}
+		// -- Connecting to SP, defer processing to parent class ------------------------------
+		HttpResponse res = super.execute(new HttpGet(SP));
+		log.info("Status: " + res.getStatusLine());
+		for (Header h:res.getAllHeaders()) {
+			log.debug(h.getName() + ": " + h.getValue());
 		}
-		finally {
-			client.close();
+		String entity = EntityUtils.toString(res.getEntity());  // Warning: This closes the getEntity() InputStream!!
+		log.debug("HttpResponse::Content: " + entity);
+
+		if (!isSamlSoapResponse(res)) {
+			throw new SOAPClientException("Service Provider not configured to accept ECP messages");
+		}
+
+		// -- Parse PAOS response -------------------------------------------------------------
+		Envelope initialLoginSoapResponse = getSoapMessage(new StringEntity(entity)); // turn the string back into an entity
+
+		// -- Pass the SOAP request from the SP to the IdP ------------------------------------
+		Envelope idpLoginSoapRequest = new EnvelopeBuilder().buildObject();
+		Body b = initialLoginSoapResponse.getBody();
+		b.detach();
+		idpLoginSoapRequest.setBody(b);
+
+		// -- Try logging in to the IdP using HTTP BASIC authentication -----------------------
+		log.debug("Logging into IdP [" + IdP + "]");
+		HttpPost idpLoginRequest = new HttpPost(IdP);
+		// Use the parent's AUTH_IN_PROGRESS string because it manages the pre-processing of a HTTP request
+		idpLoginRequest.getParams().setBooleanParameter(super.getAuthInProgress(), true);
+		idpLoginRequest.addHeader(HttpHeaders.AUTHORIZATION,
+				"Basic " + Base64.encodeBytes((username + ":" + password).getBytes()));
+		idpLoginRequest.setEntity(new StringEntity(xmlToString(idpLoginSoapRequest)));
+		HttpResponse idpLoginResponse = super.execute(idpLoginRequest);
+
+		// -- Handle HTTP log-in response from the IdP ----------------------------------------
+		log.debug("Status: " + idpLoginResponse.getStatusLine());
+		if (idpLoginResponse.getStatusLine().getStatusCode() != 200) {
+			throw new AuthenticationException(idpLoginResponse.getStatusLine().toString());
+		}
+
+		String idpEntity = EntityUtils.toString(idpLoginResponse.getEntity()); // Warning: This closes the getEntity() InputStream!!
+		log.debug("HttpResponse::Content: " + idpEntity);
+
+		// -- Parse SAML SOAP response from the IdP -------------------------------------------
+		Envelope idpLoginSoapResponse = getSoapMessage(new StringEntity(idpEntity)); // turn the string back into an entity
+		
+		// Get the consumer service URL (should be the SP's SOAP/ECP access point - we should check)
+		String assertionConsumerServiceURL = ((org.opensaml.saml2.ecp.Response) idpLoginSoapResponse.getHeader()
+				.getUnknownXMLObjects(org.opensaml.saml2.ecp.Response.DEFAULT_ELEMENT_NAME).get(0))
+				.getAssertionConsumerServiceURL();
+		log.debug("assertionConsumerServiceURL: " + assertionConsumerServiceURL);
+
+		// SAML will only use the first response in a SOAP message
+		List<XMLObject> responses = idpLoginSoapResponse.getBody().getUnknownXMLObjects(
+				Response.DEFAULT_ELEMENT_NAME);
+		if (!responses.isEmpty()) {
+			Response response = (Response) responses
+					.get(0);
+
+			// Get root code (?)
+			StatusCode sc = response.getStatus().getStatusCode();
+			while (sc.getStatusCode() != null) {
+				sc = sc.getStatusCode();
+			}
+
+			// Hm, they don't like us
+			if (StatusCode.AUTHN_FAILED_URI.equals(sc.getValue())) {
+				throw new AuthenticationException(sc.getValue());
+			}
+
+			// return the SAML response we got
+			return response;
 		}
 
 		return null;
-	}
-
-	/**
-	 * Add the ECP/PAOS headers to each outgoing request.
-	 */
-	@SuppressWarnings("deprecation")
-	private final class HttpRequestPreprocessor
-	implements HttpRequestInterceptor {
-		public void process(final HttpRequest req, final HttpContext ctx)
-				throws HttpException, IOException {
-
-			req.addHeader(HEADER_ACCEPT, MIME_TYPE_PAOS);
-			req.addHeader(HEADER_PAOS, "ver=\"" + SAMLConstants.PAOS_NS + "\";\""
-					+ SAMLConstants.SAML20ECP_NS + "\"");
-
-			HttpRequest r = req;
-			if (req instanceof HttpRequestWrapper) { // does not forward request to original
-				r = ((HttpRequestWrapper) req).getOriginal();
-			}
-
-			// This request is not redirectable, so we better knock to see if authentication
-			// is necessary.
-			if (!REDIRECTABLE.contains(r.getRequestLine().getMethod())
-					&& r.getParams().isParameterFalse(AUTH_IN_PROGRESS)) {
-				log.trace("Unredirectable request [" + r.getRequestLine().getMethod()
-						+ "], trying to knock first at " + r.getRequestLine().getUri());
-				HttpHead knockRequest = new HttpHead(r.getRequestLine().getUri());
-				client.execute(knockRequest);
-
-				for (Cookie c : cookieStore.getCookies()) {
-					log.trace(c.toString());
-				}
-				log.trace("Knocked");
-			}
-		}
 	}
 }
